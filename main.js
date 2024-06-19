3cfd52621f8ce54b0f2bfac3965696b03ee7e3f6
Kelvin-Ben <kevoben14@gmail.com>
2024-06-19 22:20:22 +0300

    Improve parts of javascript function to validate user input

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-const form = document.querySelector('.signup-form')
-const emailInput = document.getElementById('email')
-const alert = document.querySelector('.alert')
-const subscribeButton = document.querySelector('.btn-subscribe')
-
+const form = document.querySelector('.signup-form');
+const emailInput = document.getElementById('email');
+const alert = document.querySelector('.alert');
+const subscribeButton = document.querySelector('.btn-subscribe');
+const popup = document.querySelector('.popup-container')
+const successText = document.querySelector('.confirmation-text');
+const dismissButton = document.querySelector('.btn-dismiss')
 
 
 const validateEmail = (email) => {
@@ -16,23 +18,40 @@ const setEmailValidationStatus = () => {
     emailInput.style.border = 'none'
     emailInput.style.outline = '1px solid #f94144'
     alert.style.display = 'flex'
+    subscribeButton.style.backgroundColor = '#000435'
   } else {
     emailInput.style.outline = ''
     alert.style.display = 'none'
-    subscribeButton.style.backgroundColor = '#f94144'
+    subscribeButton.style.background = 'linear-gradient(to bottom, #FF6500, #f94144)'
   }
   return isValid
 }
 function handleInput() {
   setEmailValidationStatus()
 }
+function closePopup() {
+  popup.style.display = 'none'
+}
 
 function handleSubmit(e) {
   e.preventDefault(e)
+  const emailValue = emailInput.value;
 
-  // implement a check form email then show success popup component
+  if (!validateEmail(emailValue)) {
+    alert.style.display = 'flex'
+    emailInput.style.outline = '1px solid #f94144'
+    popup.style.display = 'none'
+    return
+  }
+  const formData = new FormData(e.target);
+  const data = Object.fromEntries(formData);
+
+  successText.innerHTML = `A confirmation email has been sent to <span class="bold-email">${data.email}</span>. Please open it and click the button inside to confirm your subscription.`
+  popup.style.display = 'flex'
+
   emailInput.value = ''
-  subscribeButton.style.backgroundColor = ''
+  subscribeButton.style.background = '#000435'
 }
 emailInput.addEventListener('input', handleInput)
+dismissButton.addEventListener('click', closePopup)
 form.addEventListener('submit', handleSubmit)
